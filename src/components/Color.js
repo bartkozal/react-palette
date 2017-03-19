51e3e9f832ca4b89ceaefb62e77c5fe9f2abe75f
Bartlomiej Kozal <bkzl@me.com>
2017-03-19 17:19:28 +0100

    Color layout

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
+import Palette from './Palette';
+import '../css/Color.css';
 
 class Color extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <a href="#">Remove color</a>
-          Name
-          <a href="#">Add color</a>
+      <div className="Color">
+        <header className="Color-header">
+          <a href="#">x</a>
+          #ff4136
+          <a href="#">+</a>
         </header>
-        <div>Red, Yellow, White, Grey</div>
-        <div>
-          Hue
-            <input type="range" />
-          Saturation
-            <input type="range" />
-          Lightness
-            <input type="range" />
+        <Palette colors={['#ff4136', '#ffdc00', '#dddddd', '#aaaaaa']} />
+        <div className="Color-sliders">
+          <label>Hue <span>3</span></label>
+          <input min="0" max="360" value="3" type="range" />
+          <label>Saturation <span>79</span></label>
+          <input min="0" max="100" value="79" type="range" />
+          <label>Brightness <span>100</span></label>
+          <input min="0" max="100" value="100" type="range" />
         </div>
-        <a href="#">Remove color</a>
       </div>
     );
   }
